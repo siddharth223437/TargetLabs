5ef7cb3dbea6629f6de5ab1c7180c872251497c3
sid <sid@siddharths-mbp.fios-router.home>
2018-01-10 22:10:43 -0500

    Initial checkin

---
+++ TargetLabs/src/main/java/com/tl/TargetLabsApplication.java
@@ -0,0 +1,12 @@
+package com.tl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargetLabsApplication.class, args);
+	}
+}
